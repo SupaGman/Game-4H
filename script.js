91ef31dea3cb98a6bcbb3c8ea163b2d71e7bf16a
Tanish-weebshit <93191306+Tanish-weebshit@users.noreply.github.com>
2023-06-11 15:14:25 +0000

    boss hp vreandering

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ var enem6;
 var enem7;
 var enem8;
 
-var enemHP = [100, 150, 80, 400, 100, 150, 80, 100, 200];
+var enemHP = [100, 150, 80, 400, 100, 150, 80, 100, 700];
 
 var enemTyp1;
 var enemTyp2;
